f4bdcc07bf7a54f211645764f9395b25208b38fa
cssc <992995879@qq.com>
2022-04-27 16:24:35 +0800

    添加按键监听，每隔20毫秒通过repaint（）方法调用paint方法

--- src/com/csc/Main.java
+++ src/com/csc/Main.java
@@ -1,8 +1,11 @@
 package com.csc;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         T t = new T();
-
+        while (true){
+            Thread.sleep(50);
+            t.repaint();  //每隔50毫秒调用画笔的方法
+        }
     }
 }
--- src/com/csc/T.java
+++ src/com/csc/T.java
@@ -3,6 +3,8 @@ package com.csc;
 import org.omg.CORBA.PUBLIC_MEMBER;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -19,11 +21,32 @@ public class T extends Frame {
                 System.exit(0);  //系统退出
             }
         });
+        //添加按键控制坦克的移动
+        this.addKeyListener(new MyKeyList());
     }
 
+    int x = 50,y=50;  //为了使方块能根据按键移动  设置为变量
     @Override
     public void paint(Graphics g) {  //当窗口重新绘画的时候会被调用
-        System.out.println("paint");
-        g.fillRect(50,50,50,50);
+        g.setColor(Color.BLUE);
+        g.fillRect(x,y,50,50);
+        x += 10;
+        y += 10;
+    }
+
+
+    //创建匿名内部类  处理对键盘的监听
+    class MyKeyList extends KeyAdapter{
+        @Override  //按下去 之后需要重画
+        public void keyPressed(KeyEvent e) {
+          //  x += 200;
+           // repaint(); //会默认调用paint的方法
+
+        }
+
+        @Override  //抬起来
+        public void keyReleased(KeyEvent e) {
+
+        }
     }
 }
