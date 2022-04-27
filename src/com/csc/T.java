60f1ee8b7226fe8916b381f64f65e441cbae1d96
cssc <992995879@qq.com>
2022-04-27 10:58:10 +0800

    窗口画出来了，也可以关闭

--- src/com/csc/T.java
+++ src/com/csc/T.java
@@ -1,4 +1,23 @@
 package com.csc;
 
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 public class T {
+    public static void main(String[] args) {
+        Frame f = new Frame();
+        f.setResizable(false); //设置大小不可变
+        f.setSize(1000,1000); //设置宽高
+        f.setTitle("曹顺超的坦克大战");  //设置标题
+        f.setVisible(true);  //设置窗口可见
+        //添加窗口的监听器  通过窗口的适配器
+        f.addWindowListener(new WindowAdapter() {
+            @Override   //当点小x的时候会触发
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);  //系统退出
+            }
+        });
+
+    }
 }
