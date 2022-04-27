110a6da430b64f9f0f5ee62f65983738b40bdd59
cssc <992995879@qq.com>
2022-04-27 11:23:53 +0800

    从Fream类继承，并且画一个小正方形

---
+++ src/com/csc/Main.java
@@ -0,0 +1,8 @@
+package com.csc;
+
+public class Main {
+    public static void main(String[] args) {
+        T t = new T();
+
+    }
+}
--- src/com/csc/T.java
+++ src/com/csc/T.java
@@ -1,23 +1,29 @@
 package com.csc;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class T {
-    public static void main(String[] args) {
-        Frame f = new Frame();
-        f.setResizable(false); //设置大小不可变
-        f.setSize(1000,1000); //设置宽高
-        f.setTitle("曹顺超的坦克大战");  //设置标题
-        f.setVisible(true);  //设置窗口可见
+public class T extends Frame {
+    public T(){
+        this.setResizable(false); //设置大小不可变
+        this.setSize(1000,1000); //设置宽高
+        this.setTitle("曹顺超的坦克大战");  //设置标题
+        this.setVisible(true);  //设置窗口可见
         //添加窗口的监听器  通过窗口的适配器
-        f.addWindowListener(new WindowAdapter() {
+        this.addWindowListener(new WindowAdapter() {
             @Override   //当点小x的时候会触发
             public void windowClosing(WindowEvent e) {
                 System.exit(0);  //系统退出
             }
         });
+    }
 
+    @Override
+    public void paint(Graphics g) {  //当窗口重新绘画的时候会被调用
+        System.out.println("paint");
+        g.fillRect(50,50,50,50);
     }
 }
