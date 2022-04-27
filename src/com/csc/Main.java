9841bc10a268f75bd0210aa3f2be2c6751e9e362
cssc <992995879@qq.com>
2022-04-27 10:28:41 +0800

    测试冲突

--- src/com/csc/Main.java
+++ src/com/csc/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("asd");
         System.out.println("张三");
+        System.out.println("大家啊搜的");
     }
 }
