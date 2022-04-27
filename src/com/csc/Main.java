2c0d00549676803412132edcb092be842440870b
cssc <992995879@qq.com>
2022-04-27 10:45:22 +0800

    准备开始坦克大战

--- src/com/csc/Main.java
+++ src/com/csc/Main.java
@@ -1,9 +1,5 @@
 package com.csc;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("asd");
-        System.out.println("撒吊扇灯");
-        System.out.println("这是测试");
-    }
+
 }
